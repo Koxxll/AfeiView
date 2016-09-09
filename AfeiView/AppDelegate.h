83c67fce9790c1b6df3879cb5275ee1136c40e47
koxx <912662036@qq.com>
2016-09-09 11:31:22 +0800

    Initial Commit

---
+++ AfeiView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AfeiView
+//
+//  Created by Afei on 16/9/9.
+//  Copyright © 2016年 梁玉玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AfeiView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AfeiView
+//
+//  Created by Afei on 16/9/9.
+//  Copyright © 2016年 梁玉玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
